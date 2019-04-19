de2ba498c2ee10b5802b4849664aece11e187398
LlirikOknessu <zmey64rus@gmail.com>
2019-04-19 18:34:42 +0700

    error

--- src/classes/machine_class.py
+++ src/classes/machine_class.py
@@ -1,19 +1,19 @@
 from src.functions import functions
 
+
 # Завтра еще подредачу, не помню что еще нужно добавить
 class Machine:
     # Конструктор установок
-    def __init__(self, machine_id, name, queue, broken, recipe_id):
+    def __init__(self, machine_id, name, broken):
         self.machine_id = machine_id
         self.name = name
-        self.queue = queue
+        self.queue = {}
         self.broken = broken
-        self.recipe_id = recipe_id
+        self.recipe_id = 0
         self.get_recipe()
         self.get_queue()
         self.set_recipe()
-        self.observe_next_entity()
-        print('Создана установка с id {0}: {1} name: {2}]'.format(self.part_id, self.name, self.list_id))
+        print('Создана установка с id: {0}, recipe_id: {1} name: {2}]'.format(self.machine_id, self.recipe_id, self.name))
 
     # Функция получения рецепта на установке, если мы будем цеплять его с бд (бд надо доработать)
     @functions.conn_decorator_method
@@ -32,8 +32,8 @@ class Machine:
             result = cursor.fetchone()
             # По идее я ходу увидеть полноценную очередь, но не знаю, какой вывод будет точно
             self.queue = [result['part_id'], result['recipe_id']]
-        except:
-            print('Твою мать, она не работает, мб ошибка в данных')
+        except Exception:
+            print('Твою мать, она не работает, мб ошибка в данных ', Exception)
 
     # Функция изменения рецепта на установку по первой партии в очереди
     def set_recipe(self):
--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ import pymysql.cursors
 from src.classes import part_class
 from src.functions import functions
 
-parts_set = functions.create_parts()  # Вызываем функцию создания партий
+parts_set = functions.create_machines()  # Вызываем функцию создания партий
 
 print(parts_set[14].further_time)
