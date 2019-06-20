35bc05bca9f03b1b6e04d472de0b546014cfb374
Hatyman <cC1iqgY3>
2019-06-20 13:58:12 +0700

    final

--- src/classes/part_class.py
+++ src/classes/part_class.py
@@ -1,5 +1,5 @@
 from src.functions import functions
-
+import sys
 
 class Part:
     # Конструктор партий, туда надо еще добавить параметров, вызываться он будет через функцию get_other_params
@@ -25,7 +25,11 @@ class Part:
         self.start_process = 0
         self.end_process = 0
         self.log_flag = 0
-        self.get_other_params()
+        try:
+            self.get_other_params()
+        except Exception as e:
+            print(str(e))
+            print(sys.exc_info())
         self.get_current_time()
         self.get_prev_entity()
         self.get_next_entity()
@@ -33,14 +37,14 @@ class Part:
 
     # Функция получения МВХ и установки, где партии сейчас надо быть
     @functions.conn_decorator_method
-    def get_other_params(self, cursor=None):
-        sql = "SELECT m.machines_id FROM `production`.recipe r INNER JOIN `production`.machines_has_recipe mhr ON r.recipe_id = mhr.recipe_recipe_id INNER JOIN `production`.machines m ON mhr.machines_machines_id = m.machines_id WHERE recipe_id = {0}".format(
+    def get_other_params(self, cursor=None, conn=None):
+        sql = "SELECT machines_id FROM `production`.recipe r INNER JOIN `production`.machines_has_recipe mhr ON r.recipe_id = mhr.recipe_recipe_id INNER JOIN `production`.machines m ON mhr.machines_machines_id = m.machines_id WHERE recipe_id = {0}".format(
             self.recipe_id
         )
         cursor.execute(sql)
         res = cursor.fetchone()
 
-        self.current_entity = res['machines_id']
+        self.current_entity = int(res['machines_id'])
 
     # Функция изменения оставшегося МВХ (на 20 минут) и сигнализирования, если МВХ истекает
     def dying(self):
@@ -55,7 +59,7 @@ class Part:
 
     # Функция получения времени обработки на следующей установке
     # @functions.conn_decorator_method
-    # def observe_next_entity(self, cursor=None):
+    # def observe_next_entity(self, cursor=None, conn=None):
     #
     #     # Получаем рецепт следующего шага
     #     sql = "SELECT `{0}` FROM `sosable_v0.6`.list WHERE list_id = {1}".format(int(self.act_process) + 1, self.list_id)
@@ -74,7 +78,7 @@ class Part:
 
     # Функция обновления всех (которые могут изменяться) параметров партии
     @functions.conn_decorator_method
-    def update_attr(self, cursor=None):
+    def update_attr(self, cursor=None, conn=None):
 
         sql = "SELECT active_process as act_process, queue, wait, reservation as reserve, part_recipe_id as recipe_id FROM `production`.part WHERE part_id={0}".format(
             self.part_id
@@ -100,7 +104,7 @@ class Part:
 
     # Функция получения прошлой установки
     @functions.conn_decorator_method
-    def get_prev_entity(self, cursor=None):
+    def get_prev_entity(self, cursor=None, conn=None):
         # Ищем все машины, которые могли быть по рецепту предыдущего шага
         sql = "SELECT machines_machines_id, time_limit FROM `production`.machines_has_recipe INNER JOIN `production`.recipe ON machines_has_recipe.recipe_recipe_id = recipe.recipe_id WHERE recipe_recipe_id=(SELECT `{0}` FROM `production`.list WHERE list_id={1})".format(
             int(self.act_process) - 1,
@@ -113,14 +117,16 @@ class Part:
         self.prev_entity.clear()
 
         # Обходим каждую из вернутых строк
-        for item in res:
-            self.prev_entity.append(item['machines_machines_id'])
-            self.time_limit = item['time_limit']
-            self.least_tl = item['time_limit']
+        for cash in res:
+            self.prev_entity.append(cash['machines_machines_id'])
+            if self.time_limit != cash['time_limit']:
+                self.least_tl = cash['time_limit']
+            self.time_limit = cash['time_limit']
+
 
     # Функция получения прошлой установки
     @functions.conn_decorator_method
-    def get_next_entity(self, cursor=None):
+    def get_next_entity(self, cursor=None, conn=None):
         # Ищем все машины, которые могли быть по рецепту предыдущего шага
         sql = "SELECT machines_machines_id, time_limit FROM `production`.machines_has_recipe INNER JOIN `production`.recipe ON machines_has_recipe.recipe_recipe_id = recipe.recipe_id WHERE recipe_recipe_id=(SELECT `{0}` FROM `production`.list WHERE list_id={1})".format(
             int(self.act_process) + 1,
@@ -132,15 +138,15 @@ class Part:
         # Чистим каждый раз наше множество во избежание проблем с дублированием
         self.next_entity_list.clear()
         # Обходим каждую из вернутых строк
-        for item in res:
-            self.next_entity_list.append(item['machines_machines_id'])
+        for mach in res:
+            self.next_entity_list.append(mach['machines_machines_id'])
 
     def set_next_entity(self, value):
         self.next_entity = value
 
     # Функция получения времени выполнения текущего рецепта
     @functions.conn_decorator_method
-    def get_current_time(self, cursor=None):
+    def get_current_time(self, cursor=None, conn=None):
         sql = "SELECT time_of_process FROM `production`.recipe WHERE recipe_id={0}".format(
             self.recipe_id
         )
@@ -151,17 +157,17 @@ class Part:
         return self.time_of_process
 
     @functions.conn_decorator_method  # исходя из примера метод должен обновлять очередь в базе
-    def send_queue(self, cursor=None):
+    def send_queue(self, cursor=None, conn=None):
         if self.queue:
             sql = "UPDATE `production`.part SET queue = {0} WHERE part_id = {1}".format(
                 self.queue,
                 self.part_id
             )
             cursor.execute(sql)
-
+            conn.commit()
 
     @functions.conn_decorator_method  # исходя из примера метод должен обновлять очередь в базе
-    def reset_queue(self, cursor=None):
+    def reset_queue(self, cursor=None, conn=None):
         sql = "UPDATE `production`.part SET queue = NULL WHERE part_id = {0}".format(
             self.part_id
         )
@@ -172,7 +178,7 @@ class Part:
             k_mts = self.least_tl / self.time_limit
         else:
             k_mts = 0
-        k_p = self.priority
+        k_p = self.priority / 4
         if max_next_queue:
             k_o = 1 - (next_queue / max_next_queue)
         else:
